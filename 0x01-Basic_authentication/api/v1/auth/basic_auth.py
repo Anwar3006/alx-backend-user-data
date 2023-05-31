793243ed0b1c6a1fc094c3a598b31780c87b3520
Anwar3006 <anwarsadat.d2@gmail.com>
2023-05-31 12:30:49 +0000

    auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Baic Auth class
 """
-from .auth import Auth
+from auth import Auth
 import re
 import base64
 import binascii
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -26,14 +27,14 @@ class BasicAuth(Auth):
             split = re.split("Basic\\s", authorization_header)
             for i in split[1:]:
                 return i
-    
+
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
         """
         Returns the decoded value of a Base64 string
         """
         if base64_authorization_header is None or\
-        type(base64_authorization_header) != str:
+                not isinstance(base64_authorization_header, str):
             return None
         try:
             res = base64.b64decode(
@@ -43,3 +44,16 @@ class BasicAuth(Auth):
             return res.decode('utf-8')
         except (binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """
+        Returns the user email and password from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None or\
+                not isinstance(decoded_base64_authorization_header, str) or\
+                ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        split = re.split(":", decoded_base64_authorization_header)
+        return (split[0], split[1])
